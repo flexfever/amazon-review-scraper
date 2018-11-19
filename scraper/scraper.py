ac87a92d1f21377ae99cc2b2967a088e2400b239
Brandon Vrooman <brandon_vrooman@yahoo.ca>
2018-11-19 03:32:12 -0500

    Refactor

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -14,135 +14,161 @@ class Scraper:
     def read_asins(self, asins):
         extracted_data = []
         for asin in asins:
+            amazon_url = Scraper.AMAZON_URL + asin
             print("Downloading and processing page http://www.amazon.com/dp/" + asin)
-            extracted_data.append(self.parse_reviews(asin))
+
+            page = self.load_page(amazon_url)
+            product = self.parse_product(page)
+            rating = self.parse_rating(page)
+            reviews = self.parse_reviews(page)
+
+            data = {
+                'product': product,
+                'rating': rating,
+                'reviews': reviews
+            }
+            extracted_data.append(data)
+
             sleep(5)
         f = open('data.json', 'w')
         json.dump(extracted_data, f, indent=4, ensure_ascii=False)
 
-    def parse_reviews(self, asin):
-        # This script has only been tested with Amazon.com
-        amazon_url = Scraper.AMAZON_URL + asin
+    def load_page(self, url):
         # Add some recent user agent to prevent amazon from blocking the request
         # Find some chrome user agent strings  here https://udger.com/resources/ua-list/browser-detail?browser=Chrome
         headers = {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
+        }
 
-        for i in range(5):
-
-            response = requests.get(amazon_url, headers=headers, verify=False)
+        for i in range(1):
+            response = requests.get(url, headers=headers, verify=False)
             if response.status_code == 404:
-                return {"url": amazon_url, "error": "page not found"}
+                return {"url": url, "error": "page not found"}
             if response.status_code != 200:
                 continue
+            page = response.text
+            return page
 
-            '''
-            The HTML xpath parser from lxml.etree seems to have max depth limit. 
-            It won't traverse further to parse the text if the depth exceeds 254. 
-            To avoid this, splitting the html into chunks
-            '''
-            html_chunks = response.text.split('<div id="Ask">')
+        return {
+            "error": "failed to process the page",
+            "url": url
+        }
 
-            data = {}
-            for chunks in html_chunks:
-                parser = html.fromstring(chunks)
-                XPATH_AGGREGATE = '//span[@id="acrCustomerReviewText"]'
-                XPATH_REVIEW_SECTION_1 = '//div[contains(@id,"reviews-summary")]'
-                XPATH_REVIEW_SECTION_2 = '//div[@data-hook="review"]'
 
-                XPATH_AGGREGATE_RATING = '//table[@id="histogramTable"]//tr'
-                XPATH_PRODUCT_NAME = '//h1//span[@id="productTitle"]//text()'
-                XPATH_PRODUCT_PRICE = '//span[@id="priceblock_ourprice"]/text()'
+    def parse_product(self, page):
+        parser = html.fromstring(page)
 
-                raw_product_price = parser.xpath(XPATH_PRODUCT_PRICE)
-                raw_product_name = parser.xpath(XPATH_PRODUCT_NAME)
+        XPATH_PRODUCT_NAME = '//h1//span[@id="productTitle"]//text()'
+        XPATH_PRODUCT_PRICE = '//span[@id="priceblock_ourprice"]/text()'
 
-                product_price = ''.join(raw_product_price).replace(',', '')
-                product_name = ''.join(raw_product_name).strip()
+        raw_product_price = parser.xpath(XPATH_PRODUCT_PRICE)
+        raw_product_name = parser.xpath(XPATH_PRODUCT_NAME)
 
-                data.update({"name": product_name}) if product_name else data.get("name")
-                data.update({"price": product_price}) if product_price else data.get("price")
+        product_price = ''.join(raw_product_price).replace(',', '')
+        product_name = ''.join(raw_product_name).strip()
 
-                total_ratings = parser.xpath(XPATH_AGGREGATE_RATING)
-                reviews = parser.xpath(XPATH_REVIEW_SECTION_1)
+        product = {}
+        product.update({"name": product_name}) if product_name else product.get("name")
+        product.update({"price": product_price}) if product_price else product.get("price")
 
-                if not reviews:
-                    reviews = parser.xpath(XPATH_REVIEW_SECTION_2)
-                ratings_dict = {}
-                reviews_list = []
+        return product
 
-                # grabing the rating  section in product page
-                for ratings in total_ratings:
-                    extracted_rating = ratings.xpath('./td//a//text()')
-                    if extracted_rating:
-                        rating_key = extracted_rating[0]
-                        raw_raing_value = extracted_rating[1]
-                        rating_value = raw_raing_value
-                        if rating_key:
-                            ratings_dict.update({rating_key: rating_value})
+    def parse_rating(self, page):
+        reviews_anchor = '<a id="customerReviews" class="a-link-normal" href="#"></a>'
+        html_chunks = page.split(reviews_anchor)
 
-                # Parsing individual reviews
-                for review in reviews:
-                    XPATH_RATING = './/i[@data-hook="review-star-rating"]//text()'
-                    XPATH_REVIEW_HEADER = './/a[@data-hook="review-title"]//text()'
-                    XPATH_REVIEW_POSTED_DATE = './/span[@data-hook="review-date"]//text()'
-                    XPATH_REVIEW_TEXT_1 = './/div[@data-hook="review-collapsed"]//text()'
-                    XPATH_REVIEW_TEXT_2 = './/div//span[@data-action="columnbalancing-showfullreview"]/@data-columnbalancing-showfullreview'
-                    XPATH_REVIEW_COMMENTS = './/span[@data-hook="review-comment"]//text()'
-                    XPATH_AUTHOR = './/span[contains(@class,"profile-name")]//text()'
-                    XPATH_REVIEW_TEXT_3 = './/div[contains(@id,"dpReviews")]/div/text()'
+        html_reviews = html_chunks[1]
 
-                    raw_review_author = review.xpath(XPATH_AUTHOR)
-                    raw_review_rating = review.xpath(XPATH_RATING)
-                    raw_review_header = review.xpath(XPATH_REVIEW_HEADER)
-                    raw_review_posted_date = review.xpath(XPATH_REVIEW_POSTED_DATE)
-                    raw_review_text1 = review.xpath(XPATH_REVIEW_TEXT_1)
-                    raw_review_text2 = review.xpath(XPATH_REVIEW_TEXT_2)
-                    raw_review_text3 = review.xpath(XPATH_REVIEW_TEXT_3)
+        parser = html.fromstring(html_reviews)
 
-                    # cleaning data
-                    author = ' '.join(' '.join(raw_review_author).split())
-                    review_rating = ''.join(raw_review_rating).replace('out of 5 stars', '')
-                    review_header = ' '.join(' '.join(raw_review_header).split())
+        XPATH_AGGREGATE_RATING = '//table[@id="histogramTable"]//tr'
+        total_ratings = parser.xpath(XPATH_AGGREGATE_RATING)
 
-                    try:
-                        review_posted_date = dateparser.parse(''.join(raw_review_posted_date)).strftime('%d %b %Y')
-                    except:
-                        review_posted_date = None
-                    review_text = ' '.join(' '.join(raw_review_text1).split())
+        ratings_dict = {}
 
-                    # grabbing hidden comments if present
-                    if raw_review_text2:
-                        json_loaded_review_data = json.loads(raw_review_text2[0])
-                        json_loaded_review_data_text = json_loaded_review_data['rest']
-                        cleaned_json_loaded_review_data_text = re.sub('<.*?>', '', json_loaded_review_data_text)
-                        full_review_text = review_text + cleaned_json_loaded_review_data_text
-                    else:
-                        full_review_text = review_text
-                    if not raw_review_text1:
-                        full_review_text = ' '.join(' '.join(raw_review_text3).split())
+        # grabing the rating  section in product page
+        for ratings in total_ratings:
+            extracted_rating = ratings.xpath('./td//a//text()')
+            if extracted_rating:
+                rating_key = extracted_rating[0]
+                raw_rating_value = extracted_rating[1]
+                rating_value = raw_rating_value
+                if rating_key:
+                    ratings_dict.update({rating_key: rating_value})
 
-                    raw_review_comments = review.xpath(XPATH_REVIEW_COMMENTS)
-                    review_comments = ''.join(raw_review_comments)
-                    review_comments = re.sub('[A-Za-z]', '', review_comments).strip()
-                    review_dict = {
-                        'review_comment_count': review_comments,
-                        'review_text': full_review_text,
-                        'review_posted_date': review_posted_date,
-                        'review_header': review_header,
-                        'review_rating': review_rating,
-                        'review_author': author
+        return ratings_dict
 
-                    }
-                    reviews_list.append(review_dict)
+    def parse_reviews(self, page):
+        reviews_anchor = '<a id="customerReviews" class="a-link-normal" href="#"></a>'
+        html_chunks = page.split(reviews_anchor)
 
-                data.update({
-                    'ratings': ratings_dict,
-                    'reviews': reviews_list,
-                    'url': amazon_url,
+        html_reviews = html_chunks[1]
 
-                })
-            return data
+        parser = html.fromstring(html_reviews)
+        XPATH_REVIEW_SECTION_1 = '//div[contains(@id,"reviews-summary")]'
+        XPATH_REVIEW_SECTION_2 = '//div[@data-hook="review"]'
 
-        return {"error": "failed to process the page", "url": amazon_url}
+        reviews = parser.xpath(XPATH_REVIEW_SECTION_1)
+        if not reviews:
+            reviews = parser.xpath(XPATH_REVIEW_SECTION_2)
+        reviews_list = []
 
+        # Parsing individual reviews
+        for review in reviews:
+            review_dict = self.parse_review(review)
+            reviews_list.append(review_dict)
+
+        return reviews_list
+
+    def parse_review(self, review):
+        XPATH_RATING = './/i[@data-hook="review-star-rating"]//text()'
+        XPATH_REVIEW_HEADER = './/a[@data-hook="review-title"]//text()'
+        XPATH_REVIEW_POSTED_DATE = './/span[@data-hook="review-date"]//text()'
+        XPATH_REVIEW_TEXT_1 = './/div[@data-hook="review-collapsed"]//text()'
+        XPATH_REVIEW_TEXT_2 = './/div//span[@data-action="columnbalancing-showfullreview"]/@data-columnbalancing-showfullreview'
+        XPATH_REVIEW_COMMENTS = './/span[@data-hook="review-comment"]//text()'
+        XPATH_AUTHOR = './/span[contains(@class,"profile-name")]//text()'
+        XPATH_REVIEW_TEXT_3 = './/div[contains(@id,"dpReviews")]/div/text()'
+
+        raw_review_author = review.xpath(XPATH_AUTHOR)
+        raw_review_rating = review.xpath(XPATH_RATING)
+        raw_review_header = review.xpath(XPATH_REVIEW_HEADER)
+        raw_review_posted_date = review.xpath(XPATH_REVIEW_POSTED_DATE)
+        raw_review_text1 = review.xpath(XPATH_REVIEW_TEXT_1)
+        raw_review_text2 = review.xpath(XPATH_REVIEW_TEXT_2)
+        raw_review_text3 = review.xpath(XPATH_REVIEW_TEXT_3)
+
+        # cleaning data
+        author = ' '.join(' '.join(raw_review_author).split())
+        review_rating = ''.join(raw_review_rating).replace('out of 5 stars', '')
+        review_header = ' '.join(' '.join(raw_review_header).split())
+
+        try:
+            review_posted_date = dateparser.parse(''.join(raw_review_posted_date)).strftime('%d %b %Y')
+        except:
+            review_posted_date = None
+        review_text = ' '.join(' '.join(raw_review_text1).split())
+
+        # grabbing hidden comments if present
+        if raw_review_text2:
+            json_loaded_review_data = json.loads(raw_review_text2[0])
+            json_loaded_review_data_text = json_loaded_review_data['rest']
+            cleaned_json_loaded_review_data_text = re.sub('<.*?>', '', json_loaded_review_data_text)
+            full_review_text = review_text + cleaned_json_loaded_review_data_text
+        else:
+            full_review_text = review_text
+        if not raw_review_text1:
+            full_review_text = ' '.join(' '.join(raw_review_text3).split())
+
+        raw_review_comments = review.xpath(XPATH_REVIEW_COMMENTS)
+        review_comments = ''.join(raw_review_comments)
+        review_comments = re.sub('[A-Za-z]', '', review_comments).strip()
+        review_dict = {
+            'review_comment_count': review_comments,
+            'review_text': full_review_text,
+            'review_posted_date': review_posted_date,
+            'review_header': review_header,
+            'review_rating': review_rating,
+            'review_author': author
+        }
+        return review_dict
